8092352cbc5020f57b79e5e0b0b958802937cb67
HillaryChikendu <chikenduhillarychiagoziemu@gmail.com>
2023-10-28 01:48:45 +0100

    Auth Implementations

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,6 +1,19 @@
+import { currentUser } from "@clerk/nextjs";
+
 import AccountProfile from "@/components/forms/AccountProfile";
 
-const Page = () => {
+async function Page() {
+  const user = await currentUser();
+  const userInfo = {};
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || '',
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl
+  }
+
   return (
     <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
       <h1 className="head-text"> Onboarding </h1>
@@ -9,7 +22,10 @@ const Page = () => {
       </p>
 
       <section className="mt-9 bg-dark-2 p-10">
-        <AccountProfile />
+        <AccountProfile 
+          user={userData}
+          btnTitle="Continue"
+        />
       </section>
     </main>
   )
--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,5 +1,24 @@
 "use client"
 
+import * as z from "zod"
+import { useForm } from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { UserValidation } from '@/lib/validations/user';
+import { ChangeEvent } from "react";
+import Image from 'next/image';
+
 interface Props {
     user: {
         id: string;
@@ -12,12 +31,131 @@ interface Props {
     btnTitle: string;
 }
 
+
 const AccountProfile = ({ user, btnTitle}: Props) => {
-  return (
-    <div>
-        Account Profile
-    </div>
-  )
+  const form = useForm({
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      profile_photo: user?.image || '',
+      name: user?.name || '',
+      username: user?.username || '',
+      bio: user?.bio || ''
+    }
+})
+
+const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+  e.preventDefault();
 }
 
+function onSubmit(values: z.infer<typeof UserValidation>) {
+  // Do something with the form values.
+  // ✅ This will be type-safe and validated.
+  console.log(values)
+}
+  
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="profile_photo"
+          render={({ field }) => (
+            <FormItem className="flex items-center gap-4">
+              <FormLabel className="account-form_image-label">
+                {field.value ?
+                  (<Image 
+                    src={field.value}
+                    alt='profile photo'
+                    width={96}
+                    height={96}
+                    priority
+                    className="rounded-full object-contain"
+                  />) : (
+                    <Image 
+                      src='/assets/profile.svg'
+                      alt='profile photo'
+                      width={24}
+                      height={24}
+                      className='object-contain'
+                    />
+                  )
+                }
+              </FormLabel>
+              <FormControl>
+                <Input 
+                  type="file"
+                  accept='image/*'
+                  placeholder="Upload a photo"
+                  className="account-form_image-input"
+                  onChange={(e) => handleImage(e, field.onChange)} 
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Name
+              </FormLabel>
+              <FormControl>
+                <Input
+                  type='text'
+                  className="account-form_input no-focus"
+                  {...field} 
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Username
+              </FormLabel>
+              <FormControl>
+                <Input
+                  type='text'
+                  className="account-form_input no-focus"
+                  {...field} 
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Bio
+              </FormLabel>
+              <FormControl>
+                <Textarea
+                  type='text'
+                  className="account-form_input no-focus"
+                  {...field} 
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className='bg-primary-500'>Submit</Button>
+      </form>
+    </Form>
+    )
+  }
+
 export default AccountProfile
+
+
